c9c9c9d3d26d5db52d9bbf72bcf6fbc8a9ff58ab
kunz88 <cunsolostefano@yahoo.it>
2024-11-27 13:12:33 +0100

    add: meal page

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
+import classes from "./page.module.css";
 
 const MealsPage = () => {
     return (
-        <div>
-            <p><Link href='/meals/share'>Share</Link></p>
-            <p><Link href='/meals/nsdljgnsdgljsn'>Details</Link></p>
-        </div>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Dellicius meal created{' '}
+                    <span className={classes.mai}></span>
+                </h1>
+                <p>Chose your favorites recipe and cook it yourself. It is easy to do</p>
+                <p className={classes.cta}><Link href='/meals/share'>Share Your Favorite Recipe </Link></p>
+            </header>
+            <main className={classes.main}></main>
+        </>
     );
 };
 export default MealsPage
